3cb3a531d3778995f161a7fd2eaf6c8952dd42fe
gaghackz <karanamgagan@gmail.com>
2024-10-08 08:35:31 +0530

    commit is of the happens

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # timeTableExtractorVIT
 Helps converting your VTOP timetable into an ics format so you can import it in your calender apps
+
+# How to use this code:
+
+## STEP 1:
+
+go to your VTOP timetable and right click on the timetable part and click on "Inspect" 
+
+then hover over the code which appears on the right and check every block until you see the whole timetable get highlighted in blue 
+
+when you find that code, right click on that code and click on copy --> copy outerHTML
+
+## STEP 2
+
+now paste that html you copied in a html file named as "timetable.html" and save it in the same folder as timetable.py
+
+## STEP 3
+
+now run timetable.py you should see a file timetable.ics show up in the same folder if the code executed with no errors
+
+## STEP 4
+
+now go to https://calender.google.com and click on settings and make sure your timetzone is set to IST (GMT +5:30) 
+
+now check the bottom left side for "imports/exports" and upload your ics file there and click on import
+
+# YOUR TIMETABLE SHOULD BE SUCCESFULLY IMPORTED INTO GOOGLE CALENDER BY NOW!!
--- timetable.py
+++ timetable.py
@@ -1,59 +1,97 @@
+from ics import Calendar, Event
+import datetime
 from bs4 import BeautifulSoup
-with open("timetable.html","r") as timetable:
-    soup = BeautifulSoup(timetable.read(), 'html.parser')
+import pytz  
+
+with open("timetable.html", "r") as timetable_file:
+    soup = BeautifulSoup(timetable_file.read(), 'html.parser')
 
 
-days = ["MON","TUE","WED","THU","FRI","SAT"]
-days1 = ["TUE","WED","THU","FRI","SAT"]
+ist = pytz.timezone('Asia/Kolkata')
 
-timeT = {
+days = ["MON", "TUE", "WED", "THU", "FRI", "SAT"]
+days1 = ["TUE", "WED", "THU", "FRI", "SAT"]
 
-    1: "8:00-8:50",
-    2: "9:00-9:50",
-    3: "9:00-9:50",
-    4: "10:00-10:50",
-    5: "10:00-10:50",
-    6: "11:00-11:50",
-    7: "11:00-11:50",
-    8: "12:00-12:50",
-    9: "12:00-12:50",
-    10: "13:00-13:50",
-    11: "LUNCH",
-    12: "14:00-14:50",
-    13: "14:00-14:50",
-    14: "15:00-15:50",
-    15: "15:00-15:50",
-    16: "16:00-16:50",
-    17: "16:00-16:50",
-    18: "17:00-17:50",
-    19: "17:00-17:50",
-    20: "18:00-18:50",
-    21: "19:00-19:50"
-
+timeS = {
+    3: '8:00', 4: '9:00', 5: '9:00', 6: '10:00', 7: '10:00', 
+    8: '11:00', 9: '11:00', 10: '12:00', 11: '12:00', 12: '13:00', 
+    14: '14:00', 15: '14:00', 16: '15:00', 17: '15:00', 
+    18: '16:00', 19: '16:00', 20: '17:00', 21: '17:00', 
+    22: '18:00', 23: '19:00'
 }
 
+timeE = {
+    3: '8:50', 4: '9:50', 5: '9:50', 6: '10:50', 7: '10:50', 
+    8: '11:50', 9: '11:50', 10: '12:50', 11: '12:50', 12: '13:50', 
+    14: '14:50', 15: '14:50', 16: '15:50', 17: '15:50', 
+    18: '16:50', 19: '16:50', 20: '17:50', 21: '17:50', 
+    22: '18:50', 23: '19:50'
+}
 
 timetable = {}
+counter = 3
 
-for str in soup.stripped_strings:
-    if str in days:
-        timetable[str] = []
-            
-    if str == "SAT":
-        break
+def create_raw():
+    global counter
+    for str in soup.stripped_strings:
+        if str in days:
+            timetable[str] = []
 
-results = soup.findAll("td")
-
-for j in days:
-    for i in results:
-
-        if len(i.string) >7:
-            timetable[j].append({"class":i.string})
-            results.remove(i)
-        if i.string in days1:
-            results.remove(i)
+        if str == "SAT":
             break
 
-print(timetable)
+    results = soup.findAll("td")
+    processed_indices = set()
 
+    for j in days:
+        for index, i in enumerate(results):
+            if index in processed_indices:
+                continue
 
+            if i.string and len(i.string) > 7:
+                if counter in timeS and counter in timeE:
+                    timetable[j].append({"start-time": timeS[counter], "end-time": timeE[counter], "class": i.string})
+                    processed_indices.add(index)
+
+            if i.string == "THEORY" or i.string == "LAB":
+                counter = 3
+            else:
+                counter += 1
+                if counter not in timeS or counter not in timeE:  
+                    counter = 3
+                
+
+            if i.string in days1:
+                processed_indices.add(index)
+                break
+    print(timetable)
+    create_ics()
+    
+
+def create_ics():
+    cal = Calendar()
+    start_date = datetime.datetime.now().date()
+
+    for key, value in timetable.items():
+        for i in value:
+            event = Event()  
+            event.name = i['class']
+            print(f"Event: {i['class']}, Start: {i['start-time']}, End: {i['end-time']}")
+            
+            
+            event_begin = datetime.datetime.strptime(f"{start_date} {i['start-time']}", '%Y-%m-%d %H:%M')
+            event_end = datetime.datetime.strptime(f"{start_date} {i['end-time']}", '%Y-%m-%d %H:%M')
+            
+            
+            event.begin = ist.localize(event_begin)
+            event.end = ist.localize(event_end)
+            
+            cal.events.add(event)
+
+        start_date += datetime.timedelta(days=1)
+
+    
+    with open('timetable.ics', 'w') as f:
+        f.writelines(cal)
+
+create_raw()
